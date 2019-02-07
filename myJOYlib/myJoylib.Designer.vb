8f063347e1e9c734a358d80180690e9bce6ac7dc
Alessandro Mandelli <alessandro.mandelli@costergroup.eu>
2019-02-07 16:58:13 +0100

    Recommit

--- myJOYlib/myJoylib.Designer.vb
+++ myJOYlib/myJoylib.Designer.vb
@@ -67,11 +67,12 @@ Partial Class myJOYlib
         '
         'PictureBox1
         '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PictureBox1.Image = Global.myJOYlib.My.Resources.Resources.joy1
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(94, 168)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
